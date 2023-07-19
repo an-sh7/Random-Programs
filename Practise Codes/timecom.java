f6a8d3f74951c488201fbecded307ba2d5f42056
The-Coder-AJ <anshjain722@gmail.com>
2023-07-19 23:16:55 +0530

    new file:   Practise Codes/timecom.java new file:   To-Do List/to_Do_List.java

---
+++ Practise Codes/timecom.java
@@ -0,0 +1,13 @@
+public class timecom {
+    public static void func(){
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.printf("%d %d\n", i, j);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        func();
+    }
+}
