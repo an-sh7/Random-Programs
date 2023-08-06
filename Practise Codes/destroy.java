b791c648818d38fcb70a19e6ae62abce8f4af382
The-Coder-AJ <anshjain722@gmail.com>
2023-08-06 09:48:59 +0530

    new file:   destroy.java

---
+++ Practise Codes/destroy.java
@@ -0,0 +1,10 @@
+public class destroy {
+
+    public static void main(String[] args) {
+        int i = -1;
+        while (i < 0){
+            i--;
+        }
+        System.out.println(i);
+    }
+}
